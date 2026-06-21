92103188e23d33bed78d6eafe851815dfe71b249
Saumya Jain <saumyajain@Saumyas-MacBook-Pro.local>
2026-06-22 01:47:26 +0530

    final favicon fix and cleanup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,10 +28,8 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <head>
-        <link rel="icon" href="/logo.png" />
-      </head>
       <body className="min-h-full flex flex-col">
+
         <RoleProvider>
           {children}
         </RoleProvider>
